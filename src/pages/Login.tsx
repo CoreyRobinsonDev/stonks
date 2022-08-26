d983e211a19036dd97242369b8834de64a8eac07
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-08-26 07:03:31 -0400

    proxying cors server

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,14 +18,14 @@ const Login = () => {
     e.preventDefault();
 
     if (isGuest) {
-     await axios.post("https://stonks-crd.herokuapp.com/register", {
+     await axios.post("https://cors-anywhere-crd.herokuapp.com/https://stonks-crd.herokuapp.com/register", {
         username,
         password,
         confirmPassword: password
       })
     }
 
-    axios.post("https://stonks-crd.herokuapp.com/login", {
+    axios.post("https://cors-anywhere-crd.herokuapp.com/https://stonks-crd.herokuapp.com/login", {
       username,
       password
     }).then((res) => {
