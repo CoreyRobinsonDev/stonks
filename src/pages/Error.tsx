730ae4834dbea0e95a5716ec8f28b783809bbd59
CoreyRobinsonDev <coreyrob52@gmail.com>
2022-08-28 02:31:43 -0400

    undoing what i just did

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -4,7 +4,7 @@ import ErrorCSS from "../modules/Error.module.css";
 
 const Error = () => {
   return <section className={ErrorCSS.container}>
-    <Link className={ErrorCSS.link} to="/home"><RiArrowGoBackFill/></Link>
+    <Link className={ErrorCSS.link} to="/"><RiArrowGoBackFill/></Link>
     <h1>Page not found.</h1>
   </section>
 }
